a603fc04dd49bf9e90bdfa58c3ef2086599d8d18
tairaperrault <tairaperrault@gmail.com>
2019-04-13 01:00:12 -0400

    Assignment 6 completed

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 function onReady(){
-   const toDos = [];
    const addToDoForm = document.getElementById('addToDoForm');
+   let toDos = [];
+   let id = 0;
 
    function createNewToDo() {
      const newToDoText = document.getElementById('newToDoText');
-     if (!newToDoText.value) { return; }
+     if (!newToDoText.value){ return; }
 
      toDos.push({
        title: newToDoText.value,
-       complete: false
+       complete: false,
+       id: id
      });
 
+     id++;
      newToDoText.value = '';
 
      renderTheUI();
@@ -21,23 +24,38 @@ function onReady(){
 
        toDoList.textContent = '';
 
-       toDos.forEach(function(toDo) {
+       toDos.forEach(function(toDo){
          const newLi = document.createElement('li');
          const checkbox = document.createElement('input');
          checkbox.type = "checkbox";
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete"
+
+        deleteBtn.addEventListener('click', event => {
+          toDos = toDos.filter(function(item){
+            return item.id !== toDo.id;
+          })
+
+          renderTheUI();
+
+        });
 
          newLi.textContent = toDo.title;
 
          toDoList.appendChild(newLi);
          newLi.appendChild(checkbox);
+         newLi.appendChild(deleteBtn);
        });
 }
 
-   addToDoForm.addEventListener('submit', event => {
-     event.preventDefault();
-     createNewToDo();
-   });
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
   renderTheUI();
+
 }
 
 window.onload = function() {
